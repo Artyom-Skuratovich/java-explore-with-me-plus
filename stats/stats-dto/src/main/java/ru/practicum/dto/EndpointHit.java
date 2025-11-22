6daa8cc2fcb1dd8d74c0c0a7ebf54151bbe4f75e
VlShagaliev <vl.shagaliev@yandex.ru>
2025-11-22 17:45:56 +0500

    Исправление ошибок в коде

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHit.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHit.java
@@ -1,8 +1,6 @@
 package ru.practicum.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -17,18 +15,16 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EndpointHit {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Приложение, из которого происходит вызов, должно быть указано.")
     private String app;
 
-    @NotBlank(message = "Строка вызова должна быть указана.")
     private String uri;
 
-    @NotBlank(message = "IP Адрес, откуда происходит вызов, должен быть указан.")
     private String ip;
 
-    @NotNull(message = "Время вызова должно быть указано.")
     private LocalDateTime timestamp;
 }
--- stats/stats-server/src/main/java/ru/practicum/hit/repository/HitRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/hit/repository/HitRepository.java
@@ -1,7 +1,9 @@
 package ru.practicum.hit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.dto.EndpointHit;
 
+@Repository
 public interface HitRepository extends JpaRepository<EndpointHit, Long> {
 }
