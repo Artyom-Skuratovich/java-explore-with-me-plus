293f797592bed3cf9da6dafe1bf0e14851e04dd7
GreateLordAvocado <sinner.halo@yandex.ru>
2025-12-04 23:13:13 +0700

    fix: правки EwmApplication и docker-compose

--- ewm-service/src/main/java/ru/practicum/ewm/EwmApplication.java
+++ ewm-service/src/main/java/ru/practicum/ewm/EwmApplication.java
@@ -3,7 +3,10 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "ru.practicum.ewm",
+        "ru.practicum.stats"
+})
 public class EwmApplication {
     public static void main(String[] args) {
         SpringApplication.run(EwmApplication.class, args);
