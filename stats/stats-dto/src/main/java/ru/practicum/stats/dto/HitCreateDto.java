827c73786ad76f9330c423eeab643821f3332211
Artyom Skuratovich <artyomskuratovich@gmail.com>
2025-12-10 14:27:46 +0300

    fix 1

--- stats/stats-dto/src/main/java/ru/practicum/stats/dto/HitCreateDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/HitCreateDto.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.stats.common.Constants;
 
 import java.time.LocalDateTime;
 
