827a5a00071dbc92fc37253fda0d45e23d80d43b
VlShagaliev <vl.shagaliev@yandex.ru>
2025-12-10 16:10:59 +0500

    Корректировка dockerfile

--- ewm-service/src/main/java/ru/practicum/ewm/event/service/AdminEventServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewm/event/service/AdminEventServiceImpl.java
@@ -53,13 +53,13 @@ public class AdminEventServiceImpl implements AdminEventService {
             throw new BadRequestException("The rangeStart must be earlier than or equal to the rangeEnd");
         }
 
-        statsService.sendHit(request);
+        //statsService.sendHit(request);
 
         final List<Event> events = eventRepository.findAllByCriteria(
                 users, states, categories, rangeStart, rangeEnd, PageRequest.of(from, size)
         ).stream().toList();
 
-        statsService.sendHits(events, request);
+        //statsService.sendHits(events, request);
 
         return dtoService.buildFullDtoList(events, rangeStart, rangeEnd, request.getRequestURI());
     }
@@ -101,7 +101,7 @@ public class AdminEventServiceImpl implements AdminEventService {
                 UrlUtils.removeTrailingNumberSegment(request.getRequestURI())
         );
 
-        statsService.sendHit(request);
+        //statsService.sendHit(request);
 
         return dto;
     }
